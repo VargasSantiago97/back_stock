67ddf1421088249881cd6020f003f18204db769e
Santiago <vargassantiago97@gmail.com>
2024-09-25 19:13:17 -0300

    s

--- src/routes/xlsx/ingresos.routes.js
+++ src/routes/xlsx/ingresos.routes.js
@@ -7,7 +7,7 @@ const ArticuloAsociado = require('../../model/articulosAsociados.model');
 const UnidadMedidas = require('../../model/unidadMedidas.model');
 const Rubros = require('../../model/rubros.model');
 const Subrubros = require('../../model/subrubros.model');
-
+const Depositos = require('../../model/depositos.model')
 
 const log = require('electron-log');
 const path = require('path');
@@ -65,13 +65,14 @@ const separadorCeldas = (worksheet, number) => {
         'G' + number,
         'H' + number,
         'I' + number,
-        'J' + number
+        'J' + number,
+        'K' + number
     ];
 
     range.forEach(cell => {
         worksheet.getCell(cell).border = {
             bottom: { style: 'double' },
-            color: { argb: 'FF888888'}
+            color: { argb: 'FF888888' }
         };
     });
 
@@ -96,7 +97,18 @@ router.post('/', async (req, res) => {
     const clientes = req.body.clientes
     // Crea un nuevo workbook y worksheet
     const workbook = new ExcelJS.Workbook();
-    const worksheet = workbook.addWorksheet('Hoja 1');
+    const worksheet = workbook.addWorksheet('INGRESOS Y DEVOLUCIONES');
+
+    worksheet.pageSetup.paperSize = 9;  // A4
+
+    worksheet.pageSetup.margins = {
+        left: 0.25,  // margen izquierdo
+        right: 0.25,  // margen derecho
+        top: 0.25,  // margen superior
+        bottom: 0.25,  // margen inferior
+        header: 0.1,  // margen de encabezado
+        footer: 0.1   // margen de pie de página
+    };
 
     // Estilo para el encabezado de la tabla
     const headerStyle = {
@@ -212,11 +224,13 @@ router.post('/detalles', async (req, res) => {
     const data = req.body.datos
     const fecha = req.body.fecha
     const clientes = req.body.clientes
+    const usuario = req.body.usuario
 
     //consulta db variables:
     var unidadMedidas = {}
     var rubros = {}
     var subrubros = {}
+    var depositos = {}
 
     const resultadoUM = await UnidadMedidas.findAll({
         where: {
@@ -239,13 +253,19 @@ router.post('/detalles', async (req, res) => {
     })
     resultadoSR.forEach(sr => { subrubros[sr.dataValues.id] = sr.dataValues.alias });
 
+    const resultadoD = await Depositos.findAll({
+        where: {
+            estado: 1
+        }
+    })
+    resultadoD.forEach(d => { depositos[d.dataValues.id] = d.dataValues.alias });
 
     // Crea un nuevo workbook y worksheet
     const workbook = new ExcelJS.Workbook();
-    const worksheet = workbook.addWorksheet('Hoja 1');
+    const worksheet = workbook.addWorksheet('INGRESOS Y DEVOLUCIONES');
 
     worksheet.pageSetup.paperSize = 9;  // A4
-    worksheet.pageSetup.printArea = 'A:J';
+    worksheet.pageSetup.printArea = 'A:K';
 
     worksheet.pageSetup.margins = {
         left: 0.25,  // margen izquierdo
@@ -284,28 +304,70 @@ router.post('/detalles', async (req, res) => {
         alignment: { horizontal: 'left' }
     };
 
+    const tableNumerStyle = {
+        numFmt: '#,##0.00',
+        border: {
+            top: { style: 'thin' },
+            left: { style: 'thin' },
+            bottom: { style: 'thin' },
+            right: { style: 'thin' }
+        },
+        alignment: { horizontal: 'right' }
+    };
 
-    // Información de la empresa y del cliente
-    worksheet.getCell('A1').value = modelos.listadoIngresos.razon_social;
-    worksheet.getCell('A1').font = { bold: true, size: 16 };
 
-    worksheet.getCell('A2').value = modelos.listadoIngresos.direccion;
+    //LOGO
+    // Agregar logo
+    if (modelos.detalleIngresos.img) {
+        const logoImage = workbook.addImage({
+            filename: 'src/routes/pdf/img/logo.png',
+            extension: 'png'
+        });
 
-    worksheet.getCell('A3').value = modelos.listadoIngresos.localidad;
+        // Agregar la imagen a la celda A1 con un tamaño específico
+        worksheet.addImage(logoImage, {
+            tl: { col: 0.3, row: 0 }, // Top-left corner: columna A (col 0) y fila 1 (row 0)
+            ext: { width: 68, height: 68 }, // Tamaño de la imagen, ajustando el tamaño pero manteniendo la relación de aspecto
+            editAs: 'oneCell' // Coloca la imagen en una celda sin que se deforme
+        });
+
+        // Información de la empresa y del cliente
+        worksheet.getCell('C1').value = modelos.detalleIngresos.razon_social;
+        worksheet.getCell('C1').font = { bold: true, size: 16 };
+
+        worksheet.getCell('C2').value = modelos.detalleIngresos.direccion;
+
+        worksheet.getCell('C3').value = modelos.detalleIngresos.localidad;
+    } else {
+        // Información de la empresa y del cliente
+        worksheet.getCell('A1').value = modelos.detalleIngresos.razon_social;
+        worksheet.getCell('A1').font = { bold: true, size: 16 };
+
+        worksheet.getCell('A2').value = modelos.detalleIngresos.direccion;
+
+        worksheet.getCell('A3').value = modelos.detalleIngresos.localidad;
+    }
+
 
     // Información del cliente
-    worksheet.getCell('J1').value = 'INGRESOS DE MERCADERÍA Y DEVOLUCIONES';
+    worksheet.getCell('K1').value = 'INGRESOS DE MERCADERÍA Y DEVOLUCIONES';
+    worksheet.getCell('K2').value = 'Impreso el: ' + fechaHoy();
+    worksheet.getCell('K3').value = 'Impreso por: ' + usuario;
 
-    worksheet.getCell('J1').style = {
-        font: { bold: true },
-        size: 12,
+    worksheet.getCell('K1').style = {
+        font: { size: 12, bold: true },
+        alignment: { horizontal: 'right', vertical: 'top' }
+    };
+    worksheet.getCell('K2').style = {
+        alignment: { horizontal: 'right', vertical: 'top' }
+    };
+    worksheet.getCell('K3').style = {
         alignment: { horizontal: 'right', vertical: 'top' }
     };
 
 
     worksheet.getCell('A5').value = 'Fecha: ' + fecha;
     worksheet.getCell('A6').value = 'Clientes: ' + clientes;
-
 
 
     for (const documento of data) {
@@ -337,22 +399,22 @@ router.post('/detalles', async (req, res) => {
 
         var datosDocumento = ''
         var datosEstablecimiento = ''
-        if(documento.autorizado_descripcion){
+        if (documento.autorizado_descripcion) {
             datosDocumento += "Aut.: " + documento.autorizado_descripcion
         }
-        if(documento.transporte_transporte){
-            if(datosDocumento != '') datosDocumento += ' - ';
+        if (documento.transporte_transporte) {
+            if (datosDocumento != '') datosDocumento += ' - ';
             datosDocumento += "Transp: " + documento.transporte_transporte
         }
-        if(documento.transporte_chofer){
-            if(datosDocumento != '') datosDocumento += ' - ';
+        if (documento.transporte_chofer) {
+            if (datosDocumento != '') datosDocumento += ' - ';
             datosDocumento += "Chofer: " + documento.transporte_chofer
         }
-        if(documento.transporte_patente_chasis){
-            if(datosDocumento != '') datosDocumento += ' - ';
+        if (documento.transporte_patente_chasis) {
+            if (datosDocumento != '') datosDocumento += ' - ';
             datosDocumento += "Pat: " + documento.transporte_patente_chasis + '/' + documento.transporte_patente_acoplado
         }
-        if(documento.establecimiento_descripcion){
+        if (documento.establecimiento_descripcion) {
             datosEstablecimiento = documento.establecimiento_descripcion
         }
         worksheet.addRow([datosDocumento, '', '', '', '', '', '', 'EST:', datosEstablecimiento]);
@@ -390,7 +452,8 @@ router.post('/detalles', async (req, res) => {
             'Descripcion',
             'Cant. U.F.',
             'U. Med. F.',
-            'F Vto.'
+            'F Vto.',
+            'Dep.'
         ]);
         const headerRow = worksheet.getRow(worksheet.lastRow.number);
         headerRow.eachCell((cell) => {
@@ -412,20 +475,22 @@ router.post('/detalles', async (req, res) => {
                 articulosAsociado.descripcion,
                 articulosAsociado.cantidadUnidadFundamental,
                 articulosAsociado.unidadFundamental,
-                articulosAsociado.vencimiento
+                articulosAsociado.vencimiento,
+                depositos[articulosAsociado.id_deposito] ? depositos[articulosAsociado.id_deposito] : '-'
             ]);
 
             const fila = worksheet.lastRow.number
 
-            const row = worksheet.getRow(worksheet.lastRow.number);
+            const row = worksheet.getRow(fila);
             row.eachCell((cell) => {
                 cell.style = tableCellStyle;
             });
 
-            worksheet.getCell('E' + fila).numFmt = '#,##0.00';
-            worksheet.getCell('H' + fila).numFmt = '#,##0.00';
-            worksheet.getCell('E' + fila).alignment = { horizontal: 'right' };
-            worksheet.getCell('H' + fila).alignment = { horizontal: 'right' };
+            const cellE = worksheet.getCell('E' + fila);
+            cellE.style = tableNumerStyle
+
+            const cellH = worksheet.getCell('H' + fila);
+            cellH.style = tableNumerStyle
         });
 
         var ultimaFilaArticulos = worksheet.lastRow.number
@@ -438,7 +503,7 @@ router.post('/detalles', async (req, res) => {
         worksheet.getCell('E' + (ultimaFilaArticulos + 1)).font = { bold: true }
         worksheet.getCell('H' + (ultimaFilaArticulos + 1)).font = { bold: true }
 
-        
+
         worksheet.getCell('E' + (ultimaFilaArticulos + 1)).numFmt = '#,##0.00';
         worksheet.getCell('H' + (ultimaFilaArticulos + 1)).numFmt = '#,##0.00';
 
@@ -447,12 +512,14 @@ router.post('/detalles', async (req, res) => {
             worksheet.getCell('A' + (ultimaFilaArticulos + 1)).value = 'Total Unidades: ' + documento.total_unidades
         }
         if (documento.datos.documentos) {
-            var documentacion = []
-            documento.datos.documentos.forEach(doc => {
-                documentacion.push(("(" + doc.fecha + ") " + doc.tipo + " " + doc.letra + " " + mostrarDocumento(doc.punto, doc.numero)).toLocaleUpperCase())
-            })
-            var ultimaFilaArticulos = worksheet.lastRow.number
-            worksheet.getCell('A' + (ultimaFilaArticulos + 1)).value = 'Documentacion asociada: ' + documentacion.join('; ')
+            if (documento.datos.documentos.length) {
+                var documentacion = []
+                documento.datos.documentos.forEach(doc => {
+                    documentacion.push(("(" + doc.fecha + ") " + doc.tipo + " " + doc.letra + " " + mostrarDocumento(doc.punto, doc.numero)).toLocaleUpperCase())
+                })
+                var ultimaFilaArticulos = worksheet.lastRow.number
+                worksheet.getCell('A' + (ultimaFilaArticulos + 1)).value = 'Documentacion asociada: ' + documentacion.join('; ')
+            }
         }
         if (documento.observaciones_sistema) {
             var ultimaFilaArticulos = worksheet.lastRow.number
@@ -471,13 +538,178 @@ router.post('/detalles', async (req, res) => {
         { width: 8 },  // subrubro
         { width: 10 },  // codigo
         { width: 10 },  // lote
-        { width: 15 },  // cantidad
+        { width: 13 },  // cantidad
         { width: 10 },  // um
         { width: 40 },  // desc
-        { width: 15 },   // can uf
+        { width: 13 },   // can uf
         { width: 10 },  // uf
         { width: 12 }  // vto
     ];
+
+    // Especifica los encabezados para la respuesta
+    res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    res.setHeader('Content-Disposition', 'attachment; filename=archivo.xlsx');
+
+    // Envía el archivo Excel
+    await workbook.xlsx.write(res);
+    res.end();
+});
+
+router.post('/datos', async (req, res) => {
+
+    const data = req.body.datos
+    const fecha = req.body.fecha
+    const clientes = req.body.clientes
+
+    //consulta db variables:
+    var unidadMedidas = {}
+    var rubros = {}
+    var subrubros = {}
+    var depositos = {}
+
+    const resultadoUM = await UnidadMedidas.findAll({
+        where: {
+            estado: 1
+        }
+    })
+    resultadoUM.forEach(um => { unidadMedidas[um.dataValues.id] = um.dataValues.descripcion });
+
+    const resultadoR = await Rubros.findAll({
+        where: {
+            estado: 1
+        }
+    })
+    resultadoR.forEach(r => { rubros[r.dataValues.id] = r.dataValues.descripcion });
+
+    const resultadoSR = await Subrubros.findAll({
+        where: {
+            estado: 1
+        }
+    })
+    resultadoSR.forEach(sr => { subrubros[sr.dataValues.id] = sr.dataValues.descripcion });
+
+    const resultadoD = await Depositos.findAll({
+        where: {
+            estado: 1
+        }
+    })
+    resultadoD.forEach(d => { depositos[d.dataValues.id] = d.dataValues.descripcion });
+
+    // Crea un nuevo workbook y worksheet
+    const workbook = new ExcelJS.Workbook();
+    const worksheet = workbook.addWorksheet('INGRESOS Y DEVOLUCIONES');
+
+    // Estilo para el encabezado de la tabla
+    const headerStyle = {
+        font: { bold: true },
+        alignment: { horizontal: 'center' }
+    };
+
+    // Estilo para las celdas de la tabla
+    const tableCellStyle = {
+        alignment: { horizontal: 'left' }
+    };
+
+    const tableNumerStyle = {
+        numFmt: '#,##0.00',
+        alignment: { horizontal: 'right' }
+    };
+
+    worksheet.getCell('A1').value = 'Fecha: ' + fecha;
+    worksheet.getCell('A2').value = 'Clientes: ' + clientes;
+
+    worksheet.addRow([
+        "FECHA",
+        "TIPO",
+        "PUNTO",
+        "NUMERO",
+        "COD CLIENTE",
+        "RAZON SOCIAL",
+        "AUTORIZADO",
+        "TRANSPORTE",
+        "CHOFER",
+        "PAT. CH",
+        "PAT. AC",
+        "ESTABLECIMIENTO",
+        "OBSERVACIONES",
+        "OBS SISTEMA",
+        "TOTAL UNIDADES",
+        "RUBRO",
+        "SUBRUBRO",
+        "COD ARTICULO",
+        "LOTE",
+        "CANTIDAD",
+        "AJUSTE",
+        "DOC",
+        "UNIDAD MEDIDA",
+        "ARTICULO",
+        "CANTIDAD U.F.",
+        "UNIDAD MEDIDA U.F.",
+        "VENCIMIENTO",
+        "DEPOSITO"
+    ]);
+    const headerRow = worksheet.getRow(worksheet.lastRow.number);
+    headerRow.eachCell((cell) => {
+        cell.style = headerStyle;
+    });
+
+
+    for (const documento of data) {
+
+        const resultado = await ArticuloAsociado.findAll({
+            where: {
+                estado: 1,
+                id_documento: documento.id
+            }
+        })
+
+        const articulosAsociados = resultado.map(articuloAsociado => {
+            let datosConvertidos;
+            try {
+                datosConvertidos = JSON.parse(articuloAsociado.dataValues.datos);
+            } catch (error) {
+                datosConvertidos = {};
+            }
+            return {
+                ...articuloAsociado.dataValues,
+                datos: datosConvertidos
+            };
+        });
+
+        articulosAsociados.forEach(articulosAsociado => {
+            worksheet.addRow([
+                documento.fecha,
+                documento.tipo,
+                documento.punto,
+                documento.numero,
+                documento.codigo,
+                documento.razon_social,
+                documento.autorizado_descripcion,
+                documento.transporte_transporte,
+                documento.transporte_chofer,
+                documento.transporte_patente_chasis,
+                documento.transporte_patente_acoplado,
+                documento.establecimiento_descripcion,
+                documento.observaciones,
+                documento.observaciones_sistema,
+                documento.total_unidades,
+
+                rubros[articulosAsociado.id_rubro] ? rubros[articulosAsociado.id_rubro] : articulosAsociado.id_rubro,
+                subrubros[articulosAsociado.id_subRubro] ? subrubros[articulosAsociado.id_subRubro] : articulosAsociado.id_subRubro,
+                articulosAsociado.codigo,
+                articulosAsociado.lote,
+                articulosAsociado.cantidad,
+                articulosAsociado.ajuste,
+                articulosAsociado.documento,
+                unidadMedidas[articulosAsociado.id_unidadMedida] ? unidadMedidas[articulosAsociado.id_unidadMedida] : articulosAsociado.id_unidadMedida,
+                articulosAsociado.descripcion,
+                articulosAsociado.cantidadUnidadFundamental,
+                articulosAsociado.unidadFundamental,
+                articulosAsociado.vencimiento,
+                depositos[articulosAsociado.id_deposito] ? depositos[articulosAsociado.id_deposito] : articulosAsociado.id_deposito,
+            ]);
+        });
+    }
 
     // Especifica los encabezados para la respuesta
     res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
